16e2d38a62f0ff639040e47281c700d9ea5ad3e9
Hans Buys <hans@autonomic.ai>
2019-08-08 09:13:44 -0400

    Put classes into package structure.

---
+++ src/main/java/io/integral/starter/SomeClass.java
@@ -0,0 +1,4 @@
+package io.integral.starter;
+
+public class SomeClass {
+}
--- src/test/java/io/integral/starter/test/SomeClassTest.java
+++ src/test/java/io/integral/starter/test/SomeClassTest.java
@@ -1,3 +1,6 @@
+package io.integral.starter.test;
+
+import io.integral.starter.SomeClass;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
